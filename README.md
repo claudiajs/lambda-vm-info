15ef10c39e1b5d2f9f18bf842425ddf73c5edef0
Gojko Adzic <gojko@gojko.com>
2017-01-26 23:54:22 +0100

    pkgconfig support

--- README.md
+++ README.md
@@ -3,18 +3,24 @@ Utility AWS Lambda function to show information about the container VM. It can s
 * currently installed dynamic libraries (`ldconfig -p`)
 * currently installed RPM packages (`yum list installed`)
 * environment variables
+* pkg-config versions
 
 ## Prerequisites
 
 * Node.js >= 4.3.2 (including NPM)
 * [Set up AWS access credentials for Node.js](https://claudiajs.com/tutorials/installing.html#configuring-access-credentials)
 
-## Usage
+## Installation
 
 1. Install the dependencies using `npm install`
 2. Deploy the Lambda function using `npm start`
 3. The installation procedure will print the root API URL. 
-4. Use `<URL>/ldconfig` in a browser to list active libraries
-5. Use `<URL>/packages` in a browser to list active packages
-5. Use `<URL>/env` in a browser to list environment variables
+
+## Usage
+
+* GET `<URL>/ldconfig` - list active libraries
+* GET `<URL>/packages` - list installed RPM/YUM packages
+* GET `<URL>/env` - list environment variables
+* GET `<URL>/pkg-config` - list installed development libraries with pkg-config
+* GET `<URL>/pkg-config/{package}/{command}` execute a pkg-config command, for example `/pkg-config/fontutil/modversion`
 
--- api.js
+++ api.js
@@ -1,18 +1,18 @@
 /*global require, module */
 var cpPromise = require('./child-process-promise'),
 	API = require('claudia-api-builder'),
-	api = new API();
+	api = new API(),
+	setUpHandler = function (url, command, paramsGenerator) {
+		api.get(url, function (request) {
+			'use strict';
+			return cpPromise.spawn(command, paramsGenerator(request));
+		}, {success: { contentType: 'text/plain'}});
+	};
 
-api.get('/ldconfig', function () {
-	'use strict';
-	return cpPromise.spawn('/sbin/ldconfig', ['-p']);
-}, {success: { contentType: 'text/plain'}});
-api.get('/packages', function () {
-	'use strict';
-	return cpPromise.spawn('/usr/bin/yum', ['list', 'installed']);
-}, {success: { contentType: 'text/plain'}});
-api.get('/env', function () {
-	'use strict';
-	return cpPromise.spawn('/bin/env', []);
-}, {success: { contentType: 'text/plain'}});
+setUpHandler('/ldconfig', '/sbin/ldconfig', () => ['-p']);
+setUpHandler('/packages', '/usr/bin/yum', () => ['list', 'installed']);
+setUpHandler('/pkg-config', '/usr/bin/pkg-config', () => ['--list-all']);
+setUpHandler('/pkg-config/{package}/{command}', '/usr/bin/pkg-config', (request) => ['--' + request.pathParams.command, request.pathParams.package]);
+setUpHandler('/env', '/bin/env', () => []);
+
 module.exports = api;
