c658cec4137b92561f2ea89b1b8b15709aeef999
Gojko Adzic <gojko@gojko.com>
2017-03-31 00:11:33 +0200

    show index page on root url

--- api.js
+++ api.js
@@ -2,6 +2,8 @@
 var cpPromise = require('./child-process-promise'),
 	API = require('claudia-api-builder'),
 	api = new API(),
+	fs = require('fs'),
+	path = require('path'),
 	setUpHandler = function (url, command, paramsGenerator) {
 		api.get(url, function (request) {
 			'use strict';
@@ -14,5 +16,7 @@ setUpHandler('/packages', '/usr/bin/yum', () => ['list', 'installed']);
 setUpHandler('/pkg-config', '/usr/bin/pkg-config', () => ['--list-all']);
 setUpHandler('/pkg-config/{package}/{command}', '/usr/bin/pkg-config', (request) => ['--' + request.pathParams.command, request.pathParams.package]);
 setUpHandler('/env', '/bin/env', () => []);
+api.get('/', request => '/' + request.context.stage + '/index', {success: 302});
+api.get('/index', () => fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8'), {success: {contentType: 'text/html'}});
 
 module.exports = api;
